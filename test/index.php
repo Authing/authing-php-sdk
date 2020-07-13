6a86efc9e4db84983bfe407844b5bc9783f708dd
su <recallsufuture@gmail.com>
2020-07-13 15:18:31 +0800

    feat: BREAK CHANGE
    updated a batch of interfaces and type declaration.

--- test/index.php
+++ test/index.php
@@ -6,72 +6,134 @@
  * Time: 下午11:12
  */
 
-require __DIR__.'/../vendor/autoload.php';
+require __DIR__ . '/../vendor/autoload.php';
 
-use Authing\Client;
+use Authing\AuthingApiClient;
 
+$data = [
+    'clientId' => 'your id',
+    'secret' => 'your secret',
+];
+$client = new AuthingApiClient($data);
 
-try {
-    $data = [
-        'clientId' => 'your id',
-        'secret'   => 'your secret',
-    ];
-    $client = new Client($data);
+$username = "username";
+$email = 'email';
+$password = '12345678';
 
-    /**
-     * 登录接口
-     */
-//    $client->login([
-//                       'email' => '376155014@qq.com',
-//                       'password' => '654321',
-//                   ]);
-    /**
-     * oauth 列表
-     */
-    //$client->ReadOAuthList();
-    /**
-     * 查看指定用户
-     */
-    //$client->user(['id' => '5ae816666fc94c0001569d9e'] );
-    /**
-     * 查看全部的用户
-     */
-    //$client->users(['page' => 1,'count'=> 2] );
-    /**
-     * 查看用户是否已经登录
-     */
-    //$client->checkLoginStatus();
-    /**
-     * 删除用户
-     */
-    //$client->removeUsers(['ids' => ['5ae04d373055230001023c6e']]);
-    /**
-     * 注册用户 支持多个参数
-     */
-    //$client->register(['email' => '155014@qq.com', 'password' => '123456']);
-    /**
-     * 发送确认邮箱邮件
-     */
-    //$client->sendVerifyEmail(['email' => '376155014@qq.com']);
-    /**
-     * 发送修改密码邮件
-     */
-    //$client->sendResetPasswordEmail(['email' => '376155014@qq.com']);
-    /**
-     * 确认验证码是否正确
-     */
-    //$client->verifyResetPasswordVerifyCode(['email' => '376155014@qq.com','verifyCode'=>8685]);
-    /**
-     * 修改密码
-     */
-    //$client->changePassword(['email' => '376155014@qq.com','verifyCode'=>8685,'password'=>'123456']);
-    /**
-     * 修改用户信息
-     */
-
-    //$client->updateUser(['id'=>'5ae816666fc94c0001569d9e','nickname'=>'123132','password'=>'123456']);
-
-} catch (\Exception $e) {
-    print_r($e->getMessage());
-}
-
+/**
+ * 注册用户 支持多个参数
+ */
+$param = new Authing\RegisterParam();
+$param->userInfo = new Authing\UserRegisterInput();
+$param->userInfo->username = $username;
+$param->userInfo->email = $email;
+$param->userInfo->password = $password;
+$client->register($param);
+/**
+ * 邮箱登录接口
+ */
+$param = new Authing\LoginParam();
+$param->email = $email;
+$param->password = $password;
+$res = $client->loginByEmail($param);
+$token = $res->login->token;
+$id = $res->login->_id;
+/**
+ * 用户名登录接口
+ */
+$param = new Authing\LoginParam();
+$param->username = $username;
+$param->password = $password;
+$res = $client->loginByUsername($param);
+$token = $res->login->token;
+$id = $res->login->_id;
+/**
+ * oauth 列表
+ */
+$param = new Authing\ReadOauthListParam();
+$client->readOAuthList($param);
+/**
+ * 查看指定用户
+ */
+$param = new Authing\UserParam();
+$param->id = $id;
+$client->user($param);
+/**
+ * 批量查询用户池中的用户
+ */
+$param = new Authing\UsersParam();
+$param->count = 10;
+$param->page = 1;
+$client->users($param);
+/**
+ * 查看用户是否已经登录
+ */
+$param = new Authing\CheckLoginStatusParam();
+$param->token = $token;
+$client->checkLoginStatus($param);
+/**
+ * 发送确认邮箱邮件
+ */
+$param = new Authing\SendVerifyEmailParam();
+$param->email = $email;
+$param->token = $token;
+$client->sendVerifyEmail($param);
+/**
+ * 解析 jwt token
+ */
+$param = new Authing\DecodeJwtTokenParam();
+$param->token = $token;
+$client->decodeToken($param);
+/**
+ * 检查用户是否存在
+ */
+$param = new Authing\UserExistParam();
+$param->email = $email;
+$client->userExist($param);
+/**
+ * 刷新 token
+ */
+$param = new Authing\RefreshTokenParam();
+$param->user = $id;
+$client->refreshToken($param);
+/**
+ * 根据 id 列表批量查询用户
+ */
+$param = new Authing\UserPatchParam();
+$param->ids = $id;
+$client->userPatch($param);
+/**
+ * 发送修改密码邮件
+ */
+$param = new Authing\SendResetPasswordEmailParam();
+$param->email = $email;
+$client->sendResetPasswordEmail($param);
+/**
+ * 确认验证码是否正确
+ */
+//    $param = new Authing\VerifyResetPasswordVerifyCodeParam();
+//    $param->email = $email;
+//    $param->verifyCode = "";
+//    $client->verifyResetPasswordVerifyCode($param);
+/**
+ * 修改密码
+ */
+//    $param = new Authing\ChangePasswordParam();
+//    $param->email = $email;
+//    $param->verifyCode = "";
+//    $param->password = "123456";
+//    $client->changePassword($param);
+/**
+ * 修改用户信息
+ */
+$param = new Authing\UpdateUserParam();
+$param->options = new Authing\UserUpdateInput();
+$param->options->_id = $id;
+$param->options->nickname = "hello world";
+$client->updateUser($param);
+/**
+ * 删除用户
+ */
+$param = new Authing\RemoveUsersParam();
+$param->ids = $id;
+$client->removeUsers($param);
